5fab70233e0703ac6a9b9edc6ade00f2759a5cb6
Kristen Swaim <kswaimdev@gmail.com>
2023-06-13 07:38:18 -0400

    saving changes for portability

--- ElevenNote.Data/Entities/UserEntity.cs
+++ ElevenNote.Data/Entities/UserEntity.cs
@@ -10,11 +10,15 @@ namespace ElevenNote.Data.Entities
     {
         [Key]
         public int Id { get; set;}
+        [Required] 
         public string Email {get; set;}
+        [Required] 
         public string Username {get; set;}
+        [Required] 
         public string Password {get; set;}
         public string FirstName {get; set;}
         public string LastName {get; set;}
+        [Required] 
         public DateTime DateCreated {get; set;}
     }
 }
